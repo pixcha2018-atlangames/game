162dd39fd09deeea1d84756912b9249ea34e4c80
RemiGourrierec <remi.gourrierec@free.fr>
2018-04-06 11:39:09 +0100

    ajout des persos + test graphique

---
+++ Assets/PositionCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionCamera : MonoBehaviour {
+
+    public GameObject loupGO;
+    public GameObject faonGO;
+    public float heigthCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var newPositionCam = faonGO.transform.position + (loupGO.transform.position - faonGO.transform.position)/2;
+        transform.position = newPositionCam;
+        
+        
+	}
+}
