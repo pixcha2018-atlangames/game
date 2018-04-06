4263b248b8ec0aaa5a202988e180de984b3b138e
Pierre Chabiland <pierrot@elpoyo.local>
2018-04-06 15:26:17 -0400

    camera bounds wip

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -16,8 +16,12 @@ public class Main : MonoBehaviour {
 	public Camera camera;
 	public GameObject plane;
 
+	private Vector3[] cameraBounds;
+
 	// Use this for initialization
 	void Start () {
+		cameraBounds = new Vector3[4];
+
 		storyManager = new StoryManager();
 		storyManager.Load(storyName);
 
@@ -25,36 +29,52 @@ public class Main : MonoBehaviour {
 
 		Debug.Log(storyManager.GetFirstNode());
 
+		UpdateCameraBounds();
+		Debug.Log("---------");
 
-		test(0,0);
-		test(0,1);
-		test(1,0);
-		test(1,1);
+		for(int i = 0, c=cameraBounds.Length;i<c;i++){
+			Vector3 p = cameraBounds[i];
+			
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			sphere.name = "Sphere "+i;
+			sphere.transform.position = new Vector3(p.x,p.y,p.z);
+			Debug.Log(i+" ----");
+			Debug.Log(p);
+			Debug.Log(sphere.transform.position);
+		}
+		Debug.Log("---------");
+
 
 	}
 
-	void test(float x, float y){
-		Ray ray = camera.ViewportPointToRay(new Vector3(x, y, Vector3.Distance(camera.transform.position,new Vector3(0,0,0))));
+	void UpdateCameraBounds(){
 		MeshCollider collider = plane.GetComponent<MeshCollider>();
-		RaycastHit hit;
-		collider.Raycast(ray,out hit,100f);
-		Vector3 p = hit.point;
-		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-		sphere.transform.position = p;
-
+		int i = 0;
+		for(int x = 0; x < 2;x++){
+			for(int y = 0; y < 2;y++){
+				
+				Ray ray = camera.ViewportPointToRay(new Vector3(x, y, 0));
+				RaycastHit hit;
+				collider.Raycast(ray,out hit,100f);
+				cameraBounds[i] =  hit.point;
+				i++;
+			}
+		}
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
-		if(camera == null) return;
+		//UpdateCameraBounds();
 
-		
 	}
 
 	void OnDrawGizmosSelected()
     {
-        /*Vector3 p = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(p, 0.1F);*/
+		foreach(Vector3 p in cameraBounds){
+			Gizmos.color = Color.yellow;
+        	Gizmos.DrawSphere(p, 0.1F);
+		}
     }
 }
