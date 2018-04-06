ce59f664ff08a8695d39fc4406cade8fd8c9b403
RemiGourrierec <remi.gourrierec@free.fr>
2018-04-06 21:31:12 +0100

    Jump et prefabs decors

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -11,15 +11,26 @@ public class Move : MonoBehaviour {
     public SpriteRenderer spriteUnlit;
     public SpriteRenderer spriteShadow;
     public float cold;
+    public Animator animUnlit;
+    public Animator animShadow;
+    //public GameObject foot;
+    //public GameObject empreintes;
+    public float jumpPuissance;
+    public float timerJump;
+    public float coefTimerJump = 2f;
 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         controller = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        controller = GetComponent<CharacterController>();
+        animUnlit = spriteUnlit.transform.GetComponent<Animator>();
+        animShadow = spriteShadow.transform.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         var h = Input.GetAxis("Horizontal" + playerID);
         var v = Input.GetAxis("Vertical" + playerID);
 
@@ -27,28 +38,82 @@ public class Move : MonoBehaviour {
         {
             spriteUnlit.flipX = true;
             spriteShadow.flipX = true;
-
         }
-        if (h > 0f)
+        else if (h > 0f)
         {
             spriteUnlit.flipX = false;
             spriteShadow.flipX = false;
         }
 
+        if (h == 0f && v == 0f)
+        {
+            animUnlit.SetBool("Walking", false);
+            animShadow.SetBool("Walking", false);
+        }
+        else
+        {
+            animUnlit.SetBool("Walking", true);
+            animShadow.SetBool("Walking", true);
+            //var footprint = Instantiate(foot, empreintes.transform);
+            //footprint.transform.position = transform.position;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (animUnlit.GetBool("Grelotte") == false)
+            {
+                animUnlit.SetBool("Grelotte", true);
+                animShadow.SetBool("Grelotte", true);
+            }
+            else
+            {
+                animUnlit.SetBool("Grelotte", false);
+                animShadow.SetBool("Grelotte", false);
+            }
+        }
+
 
         //transform.Translate(new Vector3(h, 0f, v) * vitesse * Time.deltaTime);
+        
+        //JUMP
+        if (Input.GetButtonDown("Jump" + playerID) && timerJump == 0f)
+        {
+            timerJump = 1f ;
+        }
+
+        if (timerJump > 0f)
+        {
+            if (timerJump - Time.deltaTime * coefTimerJump > 0f)
+            {
+                timerJump -= Time.deltaTime * coefTimerJump;
+            }
+            else timerJump = 0f;
+        }
+
 
         //DEPLACEMENT
         var moveDirection = Vector3.zero;
-        moveDirection.Set(h, 0f, v);
-        moveDirection *= vitesse;
+
+        if (timerJump != 0f)
+        {
+            animUnlit.SetBool("Jumping", true);
+            animShadow.SetBool("Jumping", true);
+            moveDirection.Set(h * (1+timerJump), jumpPuissance * timerJump, v * (1 + timerJump));
+        }
+        else
+        {
+            animUnlit.SetBool("Jumping", false);
+            animShadow.SetBool("Jumping", false);
+            moveDirection.Set(h, 0f, v);
+        }
+
+        //moveDirection.Set(h, jumpPuissance * timerJump, v);
 
         //ADD GRAVITY
         moveDirection.y += gravity;
 
         //MOVE
         controller.Move(moveDirection * Time.deltaTime);
-
 
     }
 }
